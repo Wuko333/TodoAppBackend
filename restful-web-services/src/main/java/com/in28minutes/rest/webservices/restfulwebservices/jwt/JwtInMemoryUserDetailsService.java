28df235eada31d973423250a168814ce77b7c5b6
William <wuko333@gmail.com>
2020-12-12 05:59:56 -0500

    First Commit

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/JwtInMemoryUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.in28minutes.rest.webservices.restfulwebservices.jwt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtInMemoryUserDetailsService implements UserDetailsService {
+
+  static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
+
+  static {
+    inMemoryUserList.add(new JwtUserDetails(1L, "in28minutes",
+        "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_2"));
+    inMemoryUserList.add(new JwtUserDetails(2L, "ranga",
+            "$2a$10$wPTynpUs2m1F0iCKSsw4me4wV8Ny3G0NrWh6ZWL4ZAZRQxyyJBuau", "ROLE_USER_2"));
+   
+    //$2a$10$wPTynpUs2m1F0iCKSsw4me4wV8Ny3G0NrWh6ZWL4ZAZRQxyyJBuau
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Optional<JwtUserDetails> findFirst = inMemoryUserList.stream()
+        .filter(user -> user.getUsername().equals(username)).findFirst();
+
+    if (!findFirst.isPresent()) {
+      throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
+    }
+
+    return findFirst.get();
+  }
+
+}
+
+
---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/jwt/resource/JwtTokenRequest.java
@@ -0,0 +1,42 @@
+package com.in28minutes.rest.webservices.restfulwebservices.jwt.resource;
+
+import java.io.Serializable;
+
+public class  JwtTokenRequest implements Serializable {
+  
+  private static final long serialVersionUID = -5616176897013108345L;
+
+  private String username;
+    private String password;
+    
+//    {
+//        "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJpbjI4bWludXRlcyIsImV4cCI6MTYwODAwMTU0MSwiaWF0IjoxNjA3Mzk2NzQxfQ.f-m5_uROwW0ZtRKMoRuz1d3AU7oKHgZcEdf7OJFgwAi3zTb7gFsG1-JKXD3RmHUu8i3iLxBJZM7jRZk92KqiaQ"
+//    }
+
+
+    public JwtTokenRequest() {
+        super();
+    }
+
+    public JwtTokenRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
